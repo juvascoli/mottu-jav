95095a1c89aade1ca609d1d06e583b5633506924
juvascoli <jvascoli6@gmail.com>
2025-05-25 15:11:12 -0300

    first commit

---
+++ src/main/java/br/com/fiap/challenge/model/Moto.java
@@ -0,0 +1,88 @@
+package br.com.fiap.challenge.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_moto")
+public class Moto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_moto")
+    private Long id;
+
+    @Column(length = 7)
+    private String placa;
+
+    @Column(length = 17)
+    private String chassi;
+
+    @Column(nullable = false, length = 10)
+    private String status;
+
+    @Column(nullable = false, length = 30)
+    private String modelo;
+
+    @Column(name = "id_camera", nullable = false)
+    private Integer idCamera;
+
+    public Moto() {
+    }
+
+    public Moto(Long id, String placa, String chassi, String status, String modelo, Integer idCamera) {
+        this.id = id;
+        this.placa = placa;
+        this.chassi = chassi;
+        this.status = status;
+        this.modelo = modelo;
+        this.idCamera = idCamera;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getIdCamera() {
+        return idCamera;
+    }
+
+    public void setIdCamera(Integer idCamera) {
+        this.idCamera = idCamera;
+    }
+}
